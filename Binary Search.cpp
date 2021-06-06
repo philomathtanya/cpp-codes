97b523f1a211f893ceb96aacfc99756f4a248272
philomathtanya <tanyabansal525@gmail.com>
2021-06-06 21:00:42 +0530

    Binary Search

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,53 +1,64 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int binarySearch(int[], int, int, int);
-
 int main()
 {
-   int num[10] = {10, 22, 37, 55, 92, 118};
-   int search_num, loc=-1;
-
-   cout<<"Enter the number that you want to search: ";
-   cin>>search_num;
-
-   loc = binarySearch(num, 0, 6, search_num);
-
-   if(loc != -1)
-   {
-      cout<<search_num<<" found in the array at the location: "<<loc;
-   }
-   else
-   {
-      cout<<"Element not found";
-   }
-   return 0;
-}
-
-int binarySearch(int a[], int first, int last, int search_num)
+	//input
+	int n;
+cout<<"Enter the num:";
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++)
+cin>>arr[i];
+//sort
+int min=arr[0],max=arr[0];
+for(int i=1;i<n;i++)
 {
-   int middle;
-   if(last >= first)
-   {
-      middle = (first + last)/2;
-      //Checking if the element is present at middle loc
-      if(a[middle] == search_num)
-      {
-         return middle+1;
-      }
-
-      //Checking if the search element is present in greater half
-      else if(a[middle] < search_num)
-      {
-         return binarySearch(a,middle+1,last,search_num);
-      }
-
-      //Checking if the search element is present in lower half
-      else
-      {
-         return binarySearch(a,first,middle-1,search_num);
-      }
-
-   }
-   return -1;
+	for(int j=0;j<n-i;j++)
+	{
+		if(arr[j]>arr[j+1])
+		{
+			int t=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=t;
+		}
+	}
 }
+//print
+for(int i=0;i<n;i++)
+cout<<arr[i]<<endl;
+//search
+int h,l,x;
+l=0;
+h=n-1;
+cout<<"write the element to be find";
+cin>>x;
+while(l<=h)
+{
+	int m=(l+h)/2;
+	if(x==arr[m])
+	{
+	cout<<"element at"<<m+1;
+	
+	if(x<arr[m])
+	{
+		h=m-1;//1
+	}
+	else if(x>arr[m])
+	{
+		l=m+1;
+	}
+
+	else{
+		cout<<"element not found";
+		
+	}	
+	
+	
+}
+}
+
+
+
+
+}
+
