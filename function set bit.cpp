af5c056fcc09596a5ca0c96e988c5ba6e21f723a
philomathtanya <tanyabansal525@gmail.com>
2021-06-14 21:47:50 +0530

    function set bit

---
+++ function set bit.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+int set(int b,int i)
+{
+int ls;
+ int ad;
+	ls=1<<i;
+	ad=ls | b;
+	cout<<ad;
+	
+	
+	
+	
+	
+}
+int main()
+{
+int b,c,i;
+cout<<"Enter Binary number:";
+	cin>>b;
+	cin>>i;
+set(b,i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
