e9962a3dc0582585acf7f91be3f626694a2c39f9
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-06-24 21:11:21 +0530

    password check

---
+++ password check.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string pass = "password";
+    string input;
+    cout << "What is your password: ";
+    cin >> input;
+    if (input==pass){
+        cout << "Correct" << endl;
+    }else{
+        cout << "Wrong" << endl;
+    }
+    return 0;
+}
