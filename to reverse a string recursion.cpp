16cbd15e41802fbcd4f1e00fddf8ea97b7c638c8
philomathtanya <tanyabansal525@gmail.com>
2021-07-03 22:21:33 +0530

    to reverse a string recursion

---
+++ to reverse a string recursion.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void reverse(string str){
+   if(str.size() == 0){
+      return;
+   }
+   reverse(str.substr(1));
+   cout << str[0];
+}
+int main(){
+   string a = "Arnold";
+   cout<<"recursive reverse (Arnold) ::";
+   reverse(a);
+   return 0;
+}
