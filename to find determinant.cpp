1e848b9e8ac49f726452f12f04d3c832b9715caf
philomathtanya <tanyabansal525@gmail.com>
2021-07-06 21:35:10 +0530

    to find determinant

---
+++ to find determinant.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>//aaabb
+using namespace std;
+int main()
+{
+int m,n;
+cin>>m>>n;
+int a[m][n];
+for(int i=0;i<m;i++)
+{
+	for (int j=0;j<n;j++)
+	{
+	cin>>a[i][j];	
+	}
+}
+for(int i=0;i<m;i++)
+{
+	for (int j=0;j<n;j++)
+	{
+	cout<<a[i][j]<<" ";	
+	}
+	cout<<endl;
+}
+
+int s=0;
+for(int k=0;k<m;k++)
+{
+	s=s+(a[0][k]*(a[1][(k+1)%3]*a[2][(k+2)%3] - a[1][(k+2)%3] * a[2][(k+1)%3]));
+    
+}
+cout<<"determinant is"<<s;
+}
