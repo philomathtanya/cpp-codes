a4e1fc47155eda829a156d56623a603693121057
philomathtanya <tanyabansal525@gmail.com>
2021-07-11 21:02:51 +0530

    to check uppercase

---
+++ to check uppercase.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+// Function to find string which has
+// first character of each word.
+
+char first(string str, int i=0)
+{
+
+    if (str[i] == '\0')
+
+         return 0;
+
+    if (isupper(str[i])) 
+
+            return str[i];
+
+    return first(str, i+1);
+}
+ 
+// Driver code
+
+int main()
+{
+
+    string str = "geeksforGeeKS";
+
+    char res = first(str);
+
+    if (res == 0)
+
+        cout << "No uppercase letter";
+
+    else
+
+        cout << res << "\n";
+
+    return 0;
+}
