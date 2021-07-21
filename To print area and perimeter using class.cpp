506d862cf5d4781f99852cec278406d6fbf5e16c
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-07-21 22:54:49 +0530

    To print area and perimeter using class

---
+++ To print area and perimeter using class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Triangle
+{
+public:
+    void print_area(int s1, int s2, int s3)
+    {
+        double s = (s1+s2+s3)/2.0;
+        cout << s << endl;
+        cout << "Perimeter is " << (s1+s2+s3) << endl;
+    }
+};
+
+int main()
+{
+    Triangle t;
+    t.print_area(3,4,5);
+    return 0;
+}
+//To print area and perimeter using class
