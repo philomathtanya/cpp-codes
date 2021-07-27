c118614d419eb7f83d17d82526a18523f48414be
philomathtanya <tanyabansal525@gmail.com>
2021-07-27 17:33:26 +0530

    inserting a node at last of linked list

---
+++ inserting a node at last of linked list.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+
+struct node
+{
+	int pos;
+	int data;
+	struct node *next;
+};
+struct node *head,*newnode,*temp,*p;
+head=0;
+int choice=1;
+while(choice)
+{
+	newnode=(struct node *) malloc(sizeof(struct node));
+	cout<<"enter the data"<<endl;
+	cin>>newnode->data;
+	newnode->next=0;
+	if( head==0)
+	{
+		head=temp=newnode;
+	}
+	else
+	{
+		temp->next=newnode;
+		temp=newnode;
+	}
+	cout<<"Do you want to continus(0,1) ?"<<endl;
+	cin>>choice;
+}
+p=temp;
+temp=head;
+
+//before print
+while(temp!=0)
+{
+	cout<<temp->data<<" , ";
+	temp=temp->next;
+}
+cout<<endl;
+
+
+//last insertion
+newnode=(struct node *) malloc(sizeof(struct node));
+	cout<<"enter the data"<<endl;
+	cin>>newnode->data;
+
+	newnode->next=0;
+	p->next=newnode;
+	
+	
+
+temp=head;
+
+//after print
+while(temp!=0)
+{
+	cout<<temp->data<<" , ";
+	temp=temp->next;
+}
+}
+
