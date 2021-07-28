91cd26dddeaa2cf8cff7b1fbf97db88f9f0a2bec
philomathtanya <tanyabansal525@gmail.com>
2021-07-28 17:25:44 +0530

    delete node from ending

---
+++ delete node from ending.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+struct node *head,*newnode,*temp,*p;
+head=0;
+int choice=1;
+while(choice)
+{
+	newnode=(struct node *) malloc(sizeof(struct node));
+	cout<<"enter the data"<<endl;
+	cin>>newnode->data;
+	newnode->next=0;
+	if( head==0)
+	{
+		head=temp=newnode;
+	}
+	else
+	{
+		temp->next=newnode;
+		temp=newnode;
+	}
+	cout<<"Do you want to continus(0,1) ?"<<endl;
+	cin>>choice;
+}
+temp=head;
+//before
+while(temp!=0)
+{
+	cout<<temp->data<<" , ";
+	temp=temp->next;
+}
+cout<<endl;
+//ending delete
+temp=head;
+while(temp->next !=0)
+{
+	p=temp;
+	temp=temp->next;
+	
+}
+
+p->next=0;
+free(newnode);
+
+
+
+
+temp=head;
+//after
+while(temp!=0)
+{
+	cout<<temp->data<<" , ";
+	temp=temp->next;
+}
+}
