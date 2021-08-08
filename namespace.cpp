0efaebb62abead6f9d69e0908f6f2af9fe47de27
philomathtanya <tanyabansal525@gmail.com>
2021-08-08 15:51:34 +0530

    namespace

---
+++ namespace.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+namespace ludo{
+	int a;
+	int fun();
+
+	
+}
+using namespace ludo;
+int ludo::fun()
+{
+	cout<<"i am fun";
+}
+int main()
+{
+	a=10;
+	cout<<a;
+	fun();
+}
