430c47f188db5286eca5f1856653d77dde476662
philomathtanya <tanyabansal525@gmail.com>
2021-08-19 22:40:14 +0530

    shallow copy of an objects

---
+++ shallow copy of an objects.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class dummy
+{
+	private:
+		int a , b;
+	public:
+	void set_data(int x,int y)
+	{
+	a=x;b=y;	
+	}	
+	void show_data()
+	{
+		cout<<"value of a and b is"<<a<<","<<b<<endl;
+	}
+	dummy(dummy &d)
+	{
+		a=d.a;
+		b=d.b;
+	}
+	
+};
+int main()
+{
+	dummy d1;
+	d1.set_data(3,4);
+	dummy d2=d1;
+	d2.show_data();
+}
