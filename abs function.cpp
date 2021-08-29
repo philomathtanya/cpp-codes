260476681623d980607aeee4d1291cb964ad9b88
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-08-29 21:34:36 +0530

    abs function

---
+++ abs function.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+    int val1, val2;
+  
+    /// finding absolute value using
+    /// abs() function.
+    val1 = abs(22);
+    val2 = abs(-43);
+  
+    cout << "abs(22) = " << val1 << "\n";
+    cout << "abs(-43) = " << val2 << "\n";
+    return 0;
+}
