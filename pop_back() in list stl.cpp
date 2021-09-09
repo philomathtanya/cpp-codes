755ae0c8a2622f6b2bbb64e3a6a3c7c63c898a53
philomathtanya <tanyabansal525@gmail.com>
2021-09-09 21:29:47 +0530

    pop_back() in list stl

---
+++ pop_back() in list stl.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main()
+{
+	list <int>l1{11,100,34,65,22};
+	list<int>:: iterator p=l1.begin();
+	while(p!=l1.end())
+	{
+		cout<<*p;
+		p++;
+		cout<<endl;
+	}
+	l1.pop_back();
+	list<int>:: iterator q=l1.begin();
+	while(q!=l1.end())
+	{
+		cout<<*q;
+		q++;
+		cout<<endl;
+	}
+}
