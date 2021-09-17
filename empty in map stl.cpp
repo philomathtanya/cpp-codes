499e1b9d8e04e03c1fcc982233f85027d9b9e873
philomathtanya <tanyabansal525@gmail.com>
2021-09-17 21:08:18 +0530

    empty in map stl

---
+++ empty in map stl.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+
+#include<map>
+using namespace std;
+int main()
+{
+	map<int,string>c;
+	c[100]="ritesh";
+	c[150]="tanya";
+	 map<int, string>::iterator itr;
+	  for (itr = c.begin(); itr != c.end(); ++itr) {
+        cout << '\t' << itr->first
+             << '\t' << itr->second << '\n';
+    }
+    cout<<c.empty();
+   
+}
