1ed54ef5cb0d5e1e4c5f7f05b1d4557c9a531c82
philomathtanya <tanyabansal525@gmail.com>
2021-09-18 23:02:20 +0530

    erase in string stl

---
+++ erase in string stl.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std;
+int main()
+{
+	//	c_str
+	string s1,s2;
+	s2="ritish";
+	
+	
+	s1="ritesh";
+	s1.erase();
+
+	cout<<s1;
+	
+}
