8c647b85020f163bbb4c801001e7e764bb754c40
philomathtanya <tanyabansal525@gmail.com>
2021-09-18 23:01:49 +0530

    insert string stl

---
+++ insert string stl.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std;
+int main()
+{
+	//	c_str
+	string s1,s2;
+	s2="ritish";
+	
+	
+	s1="ritesh";
+s1.insert(2,"hello bello");
+	cout<<s1;
+	
+}
