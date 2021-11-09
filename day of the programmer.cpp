915455231d579a6be51e83f35ce50df5c6733159
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-11-09 20:55:58 +0530

    day of the programmer

---
+++ day of the programmer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    cin>>year;
+     if(year==1918)
+    {
+        cout<<"26.09.1918";
+    }
+    else if((year<1918 && year%4==0) ||(year>1918  &&(year%4==0 && year%100 !=0 || year%400==0))) 
+    {
+       cout<<"12.09." + to_string(year);
+    }
+    else
+    {
+        cout<<"13.09." + to_string(year);
+    }
+
+}
