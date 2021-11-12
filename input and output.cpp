34bb63ee5804db5a403bb2e8edab4a0532b60bfd
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-11-12 22:03:33 +0530

    input and output

---
+++ input and output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int n,n1,n2;
+ cin>>n>>n1>>n2;
+   cout<<n+n1+n2;
+  
+    return 0;
+}
+
