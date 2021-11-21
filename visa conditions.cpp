6cea04d135108fdf2d6bbf4c4341e7d88e982a49
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-11-21 21:30:31 +0530

    visa conditions

---
+++ visa conditions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n;
+cin>>n;
+int arr[n][6];
+for (int i=0;i<n;i++)
+{
+    for(int j=0;j<6;j++)
+    {
+        cin>>arr[i][j];
+    }
+    
+    
+}
+for (int i=0;i<n;i++)
+{
+if((arr[i][0]<=arr[i][1])&&(arr[i][2]<=arr[i][3])&&(arr[i][4]>=arr[i][5]))
+{
+cout<<"YES \n";
+}
+else {
+    cout<<"NO \n";;
+}
+}
+	return 0;
+}
