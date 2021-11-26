72bba657ecbedb544005bd54b4cfa43257c5c858
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-11-26 20:55:23 +0530

    gfg

---
+++ Addition of two no's.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{   
+public:
+    int multiplication(int A, int B){
+      return A+B;  // code here 
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.multiplication(A,B) << endl;
+    }
+    return 0; 
+} 
